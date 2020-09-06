1100b8c6b0710bc0504e37576a4d0d68e84721da
Jack Biscupski <jack-biscupski@uiowa.edu>
2020-09-06 19:38:24 -0400

    add animation to modal

--- components/AddPoemModal/index.tsx
+++ components/AddPoemModal/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import { Button, Card, useTheme, Icon, Input, Modal, Text } from '@ui-kitten/components';
+import { View, StyleSheet, Animated } from 'react-native';
+import { Button, Card, Modal, Text } from '@ui-kitten/components';
 
 export interface Props {
     visible: Boolean,
@@ -8,20 +8,58 @@ export interface Props {
 }
 
 const AddPoemModal: React.FC<Props> = ({ visible, setVisible }) => {
+
+    const fadeAnimation = React.useRef(new Animated.Value(0)).current
+    const moveAnimation = React.useRef(new Animated.Value(0)).current
+
+    React.useEffect(() => {
+        if (visible) {
+            Animated.timing(
+                fadeAnimation,
+                {
+                    toValue: 1,
+                    duration: 1000,
+                    useNativeDriver: true
+                },
+            ).start();
+            Animated.timing(
+                moveAnimation,
+                {
+                    toValue: 1,
+                    duration: 1000,
+                    useNativeDriver: true
+                }
+            ).start();
+        }
+      }, [fadeAnimation, moveAnimation, visible])
+
+        const yVal = moveAnimation.interpolate({
+            inputRange: [0, 1],
+                outputRange: [-100, 0]
+        });
+
+        const animStyle = {
+            transform: [{
+                translateY: yVal
+            }]
+        }
+
     return (
-        <View>
-            <Modal
-                visible={visible}
-                backdropStyle={styles.backdrop}
-                onBackdropPress={() => setVisible(false)}>
-                <Card disabled={true}>
-                <Text>Welcome to UI Kitten 😻</Text>
-                <Button onPress={() => setVisible(false)}>
-                    DISMISS
-                </Button>
-                </Card>
-            </Modal>
-        </View>
+        <Modal
+            visible={visible}
+            backdropStyle={styles.backdrop}
+            onBackdropPress={() => setVisible(false)}>
+                <Animated.View
+                    style={[{ opacity: fadeAnimation }, animStyle]}
+                    >
+                    <Card disabled={true}>
+                        <Text>Welcome to UI Kitten 😻</Text>
+                        <Button onPress={() => setVisible(false)}>
+                            DISMISS
+                        </Button>
+                    </Card>
+                </Animated.View>
+        </Modal>
     )
 }
 
