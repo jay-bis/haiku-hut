9362b86c070653d529d2c3b43fc5759e49cf1e0f
Jack Biscupski <jack-biscupski@uiowa.edu>
2020-09-06 22:59:19 -0400

    add basic input for writing poems and haiku checking

--- components/AddPoemModal/index.tsx
+++ components/AddPoemModal/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { View, StyleSheet, Animated } from 'react-native';
-import { Button, Card, Modal, Text } from '@ui-kitten/components';
+import { Button, Card, Modal, Text, Input, useTheme } from '@ui-kitten/components';
+
+// old import
+const syllable = require('syllable');
 
 export interface Props {
     visible: Boolean,
@@ -33,16 +36,50 @@ const AddPoemModal: React.FC<Props> = ({ visible, setVisible }) => {
         }
       }, [fadeAnimation, moveAnimation, visible])
 
-        const yVal = moveAnimation.interpolate({
-            inputRange: [0, 1],
-                outputRange: [-100, 0]
-        });
+    const yVal = moveAnimation.interpolate({
+        inputRange: [0, 1],
+            outputRange: [-200, -90]
+    });
 
-        const animStyle = {
-            transform: [{
-                translateY: yVal
-            }]
+    const animStyle = {
+        transform: [{
+            translateY: yVal
+        }]
+    }
+
+    const theme = useTheme();
+
+    const [line1, setLine1] = React.useState<String>('');
+    const [line1Color, setLine1Color] = React.useState<String>('danger');
+    const [line2, setLine2] = React.useState<String>('');
+    const [line2Color, setLine2Color] = React.useState<String>('danger');
+    const [line3, setLine3] = React.useState<String>('');
+    const [line3Color, setLine3Color] = React.useState<String>('danger');
+
+
+    React.useEffect(() => {
+        if (syllable(line1) === 5) {
+            setLine1Color('success');
+        } else {
+            setLine1Color('danger');
         }
+    }, [line1]);
+
+    React.useEffect(() => {
+        if (syllable(line2) === 7) {
+            setLine2Color('success');
+        } else {
+            setLine2Color('danger');
+        }
+    }, [line2]);
+
+    React.useEffect(() => {
+        if (syllable(line3) === 5) {
+            setLine3Color('success');
+        } else {
+            setLine3Color('danger');
+        }
+    }, [line3]);
 
     return (
         <Modal
@@ -53,9 +90,33 @@ const AddPoemModal: React.FC<Props> = ({ visible, setVisible }) => {
                     style={[{ opacity: fadeAnimation }, animStyle]}
                     >
                     <Card disabled={true}>
-                        <Text>Welcome to UI Kitten 😻</Text>
+                        <Text style={styles.header}>Enter your poem in the lines below:</Text>
+                        <Input
+                            onChangeText={nextVal => setLine1(nextVal)}
+                            label='Line 1'
+                            style={styles.textInput}
+                            status={line1Color}
+                        >
+                        
+                        </Input>
+                        <Input
+                            onChangeText={nextVal => setLine2(nextVal)}
+                            label='Line 2'
+                            style={styles.textInput}
+                            status={line2Color}
+                        >
+                        
+                        </Input>
+                        <Input
+                            onChangeText={nextVal => setLine3(nextVal)}
+                            label='Line 3'
+                            style={styles.textInput}
+                            status={line3Color}
+                        >
+                        
+                        </Input>
                         <Button onPress={() => setVisible(false)}>
-                            DISMISS
+                            SUBMIT
                         </Button>
                     </Card>
                 </Animated.View>
@@ -67,6 +128,13 @@ const styles = StyleSheet.create({
     backdrop: {
         backgroundColor: 'rgba(0, 0, 0, 0.5)',
     },
+    header: {
+        fontFamily: 'Acre-Medium',
+        marginBottom: 12
+    },
+    textInput: {
+        margin: 10
+    }
 });
 
 export default AddPoemModal
